e75ff5b3c15d0457dfebcf1add585cd105954d3c
satoko <ksm10arstaf@gmail.com>
2025-11-08 22:58:21 +0900

    add: implement initial Tic-Tac-Toe board setup

---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    char board[3][3] = {
+        {'1', '2', '3'},
+        {'4', '5', '6'},
+        {'7', '8', '9'}
+    };
+
+
+    printf("%s", board);
+    return 0;
+}
